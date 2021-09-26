81deb14d43766b832f8d425c530941a4d05bfda8
srikarst <srikarsuri@gmail.com>
2021-09-26 10:56:57 +0530

    base publisher added

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -1,4 +1,4 @@
-import { Stan } from "node-nats-streaming";
+import { Channel } from "amqplib";
 import { Subjects } from "./subjects";
 
 interface Event {
@@ -8,21 +8,23 @@ interface Event {
 
 export abstract class Publisher<T extends Event> {
   abstract subject: T["subject"];
-  protected client: Stan;
+  protected client: Channel;
 
-  constructor(client: Stan) {
+  constructor(client: Channel) {
     this.client = client;
   }
 
-  publish(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.client.publish(this.subject, JSON.stringify(data), (err) => {
-        if (err) {
-          return reject(err);
-        }
-        console.log("Event published to subject", this.subject);
-        resolve();
-      });
-    });
+  async publish(data: T["data"]): Promise<void> {
+    // return new Promise((resolve, reject) => {
+    await this.client.assertQueue(this.subject, { durable: true });
+    this.client.sendToQueue(this.subject, Buffer.from(JSON.stringify(data)));
+    // this.client.publish(this.subject, JSON.stringify(data), (err) => {
+    //   if (err) {
+    //     return reject(err);
+    //   }
+    //   console.log("Event published to subject", this.subject);
+    //   resolve();
+    // });
+    // });
   }
 }
